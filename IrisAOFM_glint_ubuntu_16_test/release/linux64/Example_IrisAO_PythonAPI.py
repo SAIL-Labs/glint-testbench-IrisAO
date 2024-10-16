a4d928384f01f2759325a2606a0ee3d4f4d656f6
mwanakijiji <eckhart.spalding@sydney.edu.au>
2024-10-16 17:00:21 +1100

    test

--- IrisAOFM_glint_ubuntu_16_test/release/linux64/Example_IrisAO_PythonAPI.py
+++ IrisAOFM_glint_ubuntu_16_test/release/linux64/Example_IrisAO_PythonAPI.py
@@ -189,7 +189,7 @@ print "\n*** Set mirror position: all segments in loop"
 for seg_num in range(0,37):
 	raw_input("\n Press 'enter' to loop over next segment\n\n")
 	try:
-		print "Segment " +str(seg_num)+ " set to (0.10 um,0.11 mrad,0.12 mrad)"
+		print "Segment " +str(seg_num)+ " set to (0.10 um,0.41 mrad,0.3 mrad)"
 		IrisAO_API.SetMirrorPosition(mirror, seg_num, (0.10,0.11,0.12))
 		# Send the settings to the mirror
 		IrisAO_API.MirrorCommand(mirror, IrisAO_API.MirrorSendSettings)
