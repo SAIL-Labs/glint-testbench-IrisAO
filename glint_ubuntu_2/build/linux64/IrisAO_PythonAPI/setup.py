9c7e1e3d21530e5abff8a47b8d05dd48d3f1402f
Eckhart Spalding <eckhart.spalding@sydney.edu.au>
2025-01-08 11:31:56 +1100

    just before moving to Ubuntu 20.04

---
+++ glint_ubuntu_2/build/linux64/IrisAO_PythonAPI/setup.py
@@ -0,0 +1,25 @@
+#--------------------------------------------------------------
+# File setup.py
+#
+# Clement Chalumeau (SETI Institute) & Franck Marchis (Iris AO & SETI Institute)
+# Date:    Apr. 20, 2016
+# Modified Jun. 13, 2016
+#
+# used by distutils to compile the python wrapper
+#--------------------------------------------------------------
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+'''
+# test
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+'''
+
+setup(
+    name = "IrisAO_Python",
+    ext_modules = cythonize([Extension("IrisAO_Python", ["IrisAO_Python.pyx"],libraries=["irisao.devices"])]),
+)
